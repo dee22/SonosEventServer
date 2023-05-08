7abafabf90dc52bc2a7e08e41b4204909195df91
dee22 <dari231@hotmail.com>
2023-05-09 00:34:29 +0200

    revert to non docker version

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+/**
+ * Leaf API - A minimal but powerful MVC API framework
+ *
+ * @package  Leaf API
+ * @author   Michael Darko <mychi.darko@gmail.com>
+ */
 
-app()->get('/', function () {
-	response()->page('./welcome.html');
-});
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-app()->get('api/v1', function () {
-	response()->json(['message' => "Congrats!! You're on Leaf API"]);
-});
+// This file allows us to run the app from the root of the project.
+// This provides a convenient way to test your Leaf MVC app
+// without having installed a "real" web server software here.
 
-app()->run();
+// It also allows you to directly load up your application from
+// the root file for quickly hosting on shared hosting platforms.
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
+}
+
+require_once __DIR__ . '/public/index.php';
